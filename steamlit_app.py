e1507aaa59139dd50ecc783264b4c3927a112789
Sanjay <115334056+sanjaysnowflake@users.noreply.github.com>
2022-10-08 10:17:01 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('my parents new health dinner')
 streamlit.header('Breakfast menu')
-streamlit.test('Omega 3 & blueberry Oatmeal')
-streamlit.test('Kale, Spinach & rocket smooothie')
-streamlit.test('Hard-Boiled Free-range Egg')
+streamlit.text('Omega 3 & blueberry Oatmeal')
+streamlit.text('Kale, Spinach & rocket smooothie')
+streamlit.text('Hard-Boiled Free-range Egg')
