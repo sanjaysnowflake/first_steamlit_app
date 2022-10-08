7dfb9469692462d68f0fbb8408b6fad69892c618
Sanjay <115334056+sanjaysnowflake@users.noreply.github.com>
2022-10-08 10:16:05 +0530

    Update steamlit_app.py
    added breakfast menu

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('my parents new health dinner')
+streamlit.header('Breakfast menu')
+streamlit.test('Omega 3 & blueberry Oatmeal')
+streamlit.test('Kale, Spinach & rocket smooothie')
+streamlit.test('Hard-Boiled Free-range Egg')
