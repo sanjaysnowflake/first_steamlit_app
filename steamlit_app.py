c648fca32c54d1e62bab023e823ea637539003a7
Sanjay <115334056+sanjaysnowflake@users.noreply.github.com>
2022-10-18 08:04:48 +0530

    Update steamlit_app.py
    added one more header

--- steamlit_app.py
+++ steamlit_app.py
@@ -5,4 +5,4 @@ streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
 streamlit.text('🥗  Kale, Spinach & rocket smooothie')
 streamlit.text('🐔 Hard-Boiled Free-range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
